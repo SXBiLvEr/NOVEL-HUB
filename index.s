0926e38746c422b68056c26824400b2c1a9f9a9e
SXBiLvEr <167822580+SXBiLvEr@users.noreply.github.com>
2024-04-24 09:39:27 +1200

    Add files via upload

---
+++ index.s
@@ -0,0 +1,62 @@
+<div class="homepage">
+  <div class="container-2">
+    <div class="logo">
+      <div class="ellipse-2">
+      </div>
+      <div class="mask-group">
+        <img class="line-1" src="../assets/vectors/line1_x2.svg" />
+        <img class="line-2" src="../assets/vectors/line23_x2.svg" />
+        <img class="line-3" src="../assets/vectors/line35_x2.svg" />
+        <img class="line-4" src="../assets/vectors/line44_x2.svg" />
+        <img class="line-5" src="../assets/vectors/line55_x2.svg" />
+        <img class="line-6" src="../assets/vectors/line65_x2.svg" />
+        <img class="line-7" src="../assets/vectors/line7_x2.svg" />
+      </div>
+      <div class="rectangle-6">
+      </div>
+    </div>
+    <div class="navigation-bar">
+      <div class="nlh">
+        NLH
+      </div>
+      <div class="contact-us">
+        CONTACT US
+      </div>
+    </div>
+  </div>
+  <div class="page-heading">
+    <div class="divider">
+    </div>
+    <span class="novel-hub">
+      NOVEL HUB
+    </span>
+  </div>
+  <div class="partner-for-your-future-of-learning">
+    PARTNER FOR YOUR FUTURE OF LEARNING
+  </div>
+  <div class="we-look-forward-to-getting-to-know-you-and-providing-predefined-assistance-to-your-journey">
+    We look forward to getting to know you and providing predefined assistance to your journey.
+  </div>
+  <div class="container-1">
+    <span class="learn-more">
+      LEARN MORE
+    </span>
+  </div>
+  <div class="container">
+    <div class="rectangle-55">
+    </div>
+    <div class="nlh-1">
+      @NLH
+    </div>
+    <div class="rectangle-56">
+    </div>
+    <div class="nlhub">
+      @NLHUB
+    </div>
+    <div class="rectangle-57">
+    </div>
+    <div class="nlhubgmail-com">
+      NLHUB@gmail.com
+    </div>
+  </div>
+</div>
