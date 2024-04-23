eab5b048e8c2eadab3b338286ef948f8b9d86040
SXBiLvEr <167822580+SXBiLvEr@users.noreply.github.com>
2024-04-24 09:40:29 +1200

    Add files via upload

---
+++ styles.s
@@ -0,0 +1,248 @@
+.homepage .container {
+    margin-right: 31.1px;
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    width: 1116.9px;
+    box-sizing: border-box;
+  }
+  .homepage .container-1 {
+    border-radius: 55px;
+    background: #000000;
+    position: relative;
+    margin: 0 905px 92px 0;
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    padding: 19px 9.7px 25px 0;
+    width: 287px;
+    box-sizing: border-box;
+  }
+  .homepage .container-2 {
+    margin: 0 33px 48px 33px;
+    display: flex;
+    flex-direction: row;
+    align-self: flex-start;
+    width: 1311.9px;
+    box-sizing: border-box;
+  }
+  .homepage .partner-for-your-future-of-learning {
+    margin: 0 757px 18px 0;
+    display: inline-block;
+    overflow-wrap: break-word;
+    font-family: 'Outfit';
+    font-weight: 700;
+    font-size: 64px;
+    line-height: 1.563;
+    color: #E6E6E6;
+  }
+  .homepage .navigation-bar {
+    margin-bottom: 16px;
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    width: 1221.9px;
+    height: fit-content;
+    box-sizing: border-box;
+  }
+  .homepage .contact-us {
+    margin-bottom: 5px;
+    display: inline-block;
+    overflow-wrap: break-word;
+    font-family: 'Inter';
+    font-weight: 900;
+    font-size: 32px;
+    color: #FFFFFF;
+  }
+  .homepage .nlh {
+    margin: 5px 19.5px 0 0;
+    display: inline-block;
+    width: 988.5px;
+    overflow-wrap: break-word;
+    font-family: 'Inter';
+    font-weight: 700;
+    font-size: 32px;
+    color: #FFFFFF;
+  }
+  .homepage .logo {
+    border-radius: 18px;
+    position: relative;
+    margin-right: 30px;
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    padding: 19px 8.9px 23px 9px;
+    width: 60px;
+    box-sizing: border-box;
+  }
+  .homepage .ellipse-2 {
+    border-radius: 30px;
+    background: #FFFFFF;
+    position: absolute;
+    left: 50%;
+    top: 0px;
+    translate: -50% 0;
+    width: 60px;
+    height: 60px;
+  }
+  .homepage .mask-group {
+    position: relative;
+    display: flex;
+    flex-direction: row;
+    column-gap: 6.9px;
+    width: 42.1px;
+    height: fit-content;
+    box-sizing: border-box;
+  }
+  .homepage .line-1 {
+    transform: rotate(89.517deg);
+    margin: 8px 0 4px 0;
+    width: 0.1px;
+    height: 6px;
+  }
+  .homepage .line-3 {
+    transform: rotate(89.736deg);
+    margin: 5px 0 2px 0;
+    width: 0.1px;
+    height: 11px;
+  }
+  .homepage .line-2 {
+    transform: rotate(89.517deg);
+    margin: 8px 0 4px 0;
+    width: 0.1px;
+    height: 6px;
+  }
+  .homepage .line-4 {
+    transform: rotate(89.839deg);
+    width: 0.1px;
+    height: 18px;
+  }
+  .homepage .line-5 {
+    transform: rotate(89.736deg);
+    margin: 5px 0 2px 0;
+    width: 0.1px;
+    height: 11px;
+  }
+  .homepage .line-6 {
+    transform: rotate(89.517deg);
+    margin: 8px 0 4px 0;
+    width: 0.1px;
+    height: 6px;
+  }
+  .homepage .line-7 {
+    transform: rotate(89.517deg);
+    margin: 8px 0 4px 0;
+    width: 0.1px;
+    height: 6px;
+  }
+  .homepage .rectangle-6 {
+    mix-blend-mode: overlay;
+    border-radius: 18px;
+    background: linear-gradient(180deg, #000000, #FFFFFF, #737373);
+    position: absolute;
+    top: 17px;
+    right: 3px;
+    width: 53px;
+    height: 22px;
+  }
+  .homepage .rectangle-55 {
+    background: url('../assets/images/rectangle60.png') 50% / cover no-repeat;
+    margin-top: 4px;
+    width: 48px;
+    height: 48px;
+  }
+  .homepage .rectangle-56 {
+    background: url('../assets/images/rectangle59.png') 50% / cover no-repeat;
+    margin-bottom: 4px;
+    width: 48px;
+    height: 48px;
+  }
+  .homepage {
+    background: url('../assets/images/homepage.jpeg') 50% / cover no-repeat;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    padding: 66px 57px 122px 57px;
+    width: 1604px;
+    box-sizing: border-box;
+  }
+  .homepage .rectangle-57 {
+    background: url('../assets/images/rectangle58.png') 50% / cover no-repeat;
+    margin: 1px 0 3px 0;
+    width: 48px;
+    height: 48px;
+  }
+  .homepage .nlh-1 {
+    margin: 4px 0 10px 0;
+    display: inline-block;
+    overflow-wrap: break-word;
+    font-family: 'Inria Serif';
+    font-weight: 400;
+    font-size: 32px;
+    color: #FFFFFF;
+  }
+  .homepage .nlhub {
+    margin: 6px 0 8px 0;
+    display: inline-block;
+    overflow-wrap: break-word;
+    font-family: 'Inria Serif';
+    font-weight: 400;
+    font-size: 32px;
+    color: #FFFFFF;
+  }
+  .homepage .nlhubgmail-com {
+    margin: 3px 0 11px 0;
+    display: inline-block;
+    overflow-wrap: break-word;
+    font-family: 'Inria Serif';
+    font-weight: 400;
+    font-size: 32px;
+    color: #FFFFFF;
+  }
+  .homepage .page-heading {
+    border-radius: 60px;
+    background: #FFFFFF;
+    position: relative;
+    margin: 0 2px 13px 0;
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    padding: 16px 23.9px 20px 0;
+    width: 1488px;
+    box-sizing: border-box;
+  }
+  .homepage .divider {
+    box-shadow: 0px 4px 4px 0px rgba(0, 0, 0, 0.25);
+    background: #FFFFFF;
+    position: absolute;
+    left: 96px;
+    right: 96px;
+    bottom: -2px;
+    height: 2px;
+  }
+  .homepage .novel-hub {
+    overflow-wrap: break-word;
+    font-family: 'Inria Serif';
+    font-style: italic;
+    font-weight: 700;
+    font-size: 96px;
+    line-height: 1.3;
+    color: #000000;
+  }
+  .homepage .we-look-forward-to-getting-to-know-you-and-providing-predefined-assistance-to-your-journey {
+    margin: 0 812.1px 80px 0;
+    display: inline-block;
+    overflow-wrap: break-word;
+    font-family: 'Inter';
+    font-weight: 400;
+    font-size: 32px;
+    color: #FFFFFF;
+  }
+  .homepage .learn-more {
+    overflow-wrap: break-word;
+    font-family: 'Outfit';
+    font-weight: 600;
+    font-size: 32px;
+    text-transform: uppercase;
+    color: #FFFFFF;
+  }
